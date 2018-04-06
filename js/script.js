7788a0c0de14ff2ca12d27b615560fc4fb1feccd
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2018-04-06 03:41:26 -0300

    arquivo 'script.js' reorganizado, criado funcoes para retorno de usuarios

--- js/script.js
+++ js/script.js
@@ -12,26 +12,87 @@ function getConfig(){
 	return config;
 };
 
-function writeUserData(database, userId, name, email) {
-  	database.ref('teste/' + userId).set({
+function writeUserData(database, name, email) {
+	
+	// Cria uma referencia para o nó 'users'
+	var users = database.ref('users');
+	userId = users.numChildren() + 1;
+  	users.ref('teste/' + userId).set({
 	    username: name,
 	    email: email
 	});
 }
 
-function firebaseteste(firebase, id, msg) {
-	// body...
-	var commentsRef = firebase.ref('teste/' + id);
-	commentsRef.on('child_added', function(data) {
-	  addCommentElement(msg);
-	});
-	/*
-	commentsRef.on('child_changed', function(data) {
-	  setCommentValues(postElement, data.key, data.val().text, data.val().author);
+function _initFirebase(database){
+
+	// Cria uma referencia para o nó 'users'
+	var users = database.ref('users');
+
+	// Retorna os 3 primeiros usuarios
+	//var users = database.ref('users').limitToFirst(3);
+
+	// Retorna os 3 ultimos usuarios
+	// var users = database.ref('users').limitToLast(3);
+
+	// Retorna todos os itens com chave/valor que começa com 'got'
+	// Testes não concluidos
+	// var users = database.ref('users').startAt('got');
+
+	// Retorna todos os itens com chave/valor que terminam com 'ria'
+	// Testes não concluidos
+	// var users = database.ref('users').endAt('ria');
+
+	// Retorna todos os itens com chave/valor iguais a 'teste 1'
+	// Testes não concluidos
+	// var users = database.ref('users').equalTo('teste 1');
+
+	// Retorna dados ordenados por ''
+	// Testes não concluidos
+	// var users = database.ref('users').orderByChild();
+
+	// Retorna dados ordenado pela chave
+	// Testes não concluidos
+	// var users = database.ref('users').orderByKey();
+
+	// Retorna dados ordenados pelo valor 
+	// Testes não concluidos
+	// var users = database.ref('users').orderByValue();
+
+
+	// Atualiza lista quando um usuario é adicionado
+	users.on('child_added', function(data){    
+		$("#data").append(addP(data.key, data.val().name, data.val().email || ""));;
+	});	
+
+	// Atualiza lista quando um usuario é deletado
+	users.on('child_removed', function(data) {
+		$("#"+data.key).remove();
 	});
 
-	commentsRef.on('child_removed', function(data) {
-	  deleteComment(postElement, data.key);
-	});*/
+	// Atualiza a lista quando um usuario é modificado
+	users.on('child_changed', function(data){
+		$("#"+data.key).remove();
+		$("#data").append(addP(data.key, data.val().name, data.val().email || ""));;
+	});			
 }
 
+function addP(key, name, email){
+	return "<p id=\""+key+"\">"+name+"<br>"+email+"</p>";
+}
+
+function removeUsers(){
+	firebase.database().ref('users/'+userId);
+	adaRef.remove()
+	  .then(function() {
+	    console.log("Remove succeeded.")
+	  })
+	  .catch(function(error) {
+	    console.log("Remove failed: " + error.message)
+	  });
+}
+
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
